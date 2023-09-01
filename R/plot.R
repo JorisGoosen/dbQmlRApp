894f693f0848affca283908e1864d91d191ef043
Joris Goosen <joris@jorisgoosen.nl>
2023-09-01 16:55:56 +0200

    twee plots in 1 bestand :o

--- R/plot.R
+++ R/plot.R
@@ -1,17 +1,42 @@
-#df		<- SchoolScannerTextOnly[c("graagNaarSchool", "bangOpSchool")]
 
-dfGraag <- data.frame(table(SchoolScannerTextOnly$graagNaarSchool))
-dfBang  <- data.frame(table(SchoolScannerTextOnly$bangOpSchool))
+#welkPlot <- "horizontaalPerLabel"
+welkPlot <- "horizontaalMeerdere"
 
-dfGraag$kolom <- "graagNaarSchool"
-dfBang$kolom  <- "bangOpSchool"
+plot <- switch(welkPlot,
 
-df <- rbind(dfGraag, dfBang)
-df$Freq <- df$Freq / length(SchoolScannerTextOnly$graagNaarSchool)
+horizontaalPerLabel =
+{
+    tafelPerGender  <- table(SchoolScannerTextOnly[c('graagNaarSchool', 'gender')])
+	dfPer		        <- as.data.frame(tafelPerGender)
 
-#levels moeten gesorteerd enzo
-lelijkeBarChart <- ggplot(df, aes(x=kolom, y=Freq, fill=Var1)) + geom_bar(position="fill", stat="identity") + coord_flip()
+    hoeVaaks        <- levels(as.factor(SchoolScannerTextOnly$graagNaarSchool))
+	genders         <- levels(as.factor(SchoolScannerTextOnly$gender))
+	dfGender	      <- table(SchoolScannerTextOnly$gender)
 
-writeImage(plot=lelijkeBarChart, plotFolder=PLOTFOLDER,	plotFile=PLOTFILE,	width=WIDTH, height=HEIGHT)
+    dfPer           <- dfPer %>% rowwise() %>% select(gender, graagNaarSchool, Freq) %>%  mutate(`Hoe vaak` = 100.0 * Freq / dfGender[[gender]], Gender=gender, `Graag naar school`=graagNaarSchool)
+
+    ggplot(dfPer, aes(x=Gender, y=`Hoe vaak`, fill=`Graag naar school`)) + geom_bar(position="fill", stat="identity") + coord_flip()
+},
+horizontaalMeerdere=
+{
+
+    dfGraag <- as.data.frame(table(SchoolScannerTextOnly$graagNaarSchool))
+	dfBang  <- as.data.frame(table(SchoolScannerTextOnly$bangOpSchool))
+
+    dfGraag$Kolom <- "Graag naar school"
+	dfBang$Kolom  <- "Bang op school"
+
+    df <- rbind(dfGraag, dfBang)
+
+    df <- df %>% rowwise() %>% select(Var1, Freq, Kolom) %>% mutate(`Procent`=100.0 * Freq / length(SchoolScannerTextOnly$graagNaarSchool), `Hoe vaak`=Var1)
+
+
+    #levels moeten gesorteerd enzo
+	ggplot(df, aes(x=Kolom, y=`Procent`, fill=`Hoe vaak`)) + geom_bar(position="fill", stat="identity") + coord_flip()
+}
+)
+
+
+writeImage(plot=plot, plotFolder=PLOTFOLDER,	plotFile=PLOTFILE,	width=WIDTH, height=HEIGHT)
 
 #check deze: https://r-graph-gallery.com/48-grouped-barplot-with-ggplot2
